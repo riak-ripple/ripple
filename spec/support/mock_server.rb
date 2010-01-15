4223b5777c9e64bd8195b23e33a26f135971d771
Sean Cribbs <seancribbs@gmail.com>
2010-01-15 15:54:06 -0500

    Add license statement to top of all appropriate files.

--- spec/support/mock_server.rb
+++ spec/support/mock_server.rb
@@ -1,30 +1,21 @@
-# -*- coding: utf-8 -*-
+# Copyright 2010 Sean Cribbs, Sonian Inc., and Basho Technologies, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Based on code from Rob Styles and Chris Tierney found at:
+#   http://dynamicorange.com/2009/02/18/ruby-mock-web-server/
 require 'rack'
-#
-## Bring up server in a new thread (do once?):
-# @mock_server = MockServer.new(4000, 0.5)
-#
-#
-## Pull down server:
-# @mock_server.stop
-#
-#
-## Expectations (rspec example):
-# request_received = false
-# @mock_server.attach do |env|
-# request_received = true
-# env['REQUEST_METHOD'].should == ‘POST’
-# env['PATH_INFO'].should == ‘/foo’
-# [ 200, { 'Content-Type' => 'text/plain', 'Content-Length' => '40' }, [ 'This gets returned from the HTTP request' ]]
-# end
-# request_received.should be_true
-# my_code_that_should_make_post_request # to http://localhost:4000/foo
-#
-#
-## After each test:
-# @mock_server.detach
-#
-#
+
 class MockServer
   def initialize(port = 4000, pause = 1)
     @block = nil
