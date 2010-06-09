418359bcbbf06c8adaf57187b3007fa3a232d03f
Sean Cribbs <seancribbs@gmail.com>
2010-06-09 15:42:26 -0400

    Quorum options for deletes need to be optional.

--- riak-client/lib/riak/robject.rb
+++ riak-client/lib/riak/robject.rb
@@ -141,7 +141,7 @@ module Riak
 
     # Delete the object from Riak and freeze this instance.  Will work whether or not the object actually
     # exists in the Riak database.
-    def delete(options)
+    def delete(options={})
       return if key.blank?
       @bucket.delete(key, options)
       freeze
