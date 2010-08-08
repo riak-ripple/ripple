1f3d38c7748c8ed036fbd0c93478b1f91a3f412d
Sean Cribbs <seancribbs@gmail.com>
2010-08-08 16:41:54 -0400

    Check version of AS before requiring. Closes #46.

--- riak-client/lib/riak/i18n.rb
+++ riak-client/lib/riak/i18n.rb
@@ -11,9 +11,9 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-begin
+if ActiveSupport::VERSION::STRING >= "3.0"
   require 'active_support/i18n'
-rescue LoadError
+else
   require 'i18n' # support ActiveSupport < 3
 end
 
