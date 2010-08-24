bf73ef609d2be63ca76fa87fa6ff5ac0737e631c
Sean Cribbs <seancribbs@gmail.com>
2010-08-24 12:17:18 -0400

    Split headers for net/http more reliably.

--- riak-client/lib/riak/util/headers.rb
+++ riak-client/lib/riak/util/headers.rb
@@ -13,6 +13,26 @@
 #    limitations under the License.
 require 'riak'
 
+# Splits headers into < 8KB chunks
+module Net::HTTPHeader
+  def each_capitalized
+    # 1.9 check
+    respond_to?(:enum_for) and (block_given? or return enum_for(__method__))
+    @header.each do |k,v|
+      base_length = "#{k}: \r\n".length
+      values = v.map {|i| i.split(", ") }.flatten
+      while !values.empty?
+        current_line = ""
+        while values.first && current_line.length + base_length + values.first.length + 2 < 8192
+          val = values.shift.strip
+          current_line += current_line.empty? ? val : ", #{val}"
+        end
+        yield capitalize(k), current_line
+      end
+    end
+  end
+end
+
 module Riak
   module Util
     # Represents headers from an HTTP response
