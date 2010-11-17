a2231f9c61f790a1c5f0834fa31dc472918c2966
Sean Cribbs <seancribbs@gmail.com>
2010-11-17 10:47:35 -0500

    Make CacheStore specs run again.

--- riak-client/lib/riak/cache_store.rb
+++ riak-client/lib/riak/cache_store.rb
@@ -12,6 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 require 'riak'
+require 'active_support/cache'
 
 module Riak
   # An ActiveSupport::Cache::Store implementation that uses Riak.
