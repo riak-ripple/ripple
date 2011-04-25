93577527ea73f6b119452aba460e34b0331d7933
Duff OMelia <duff@omelia.org>
2011-04-25 10:24:28 -0400

    Remove license from each file.
    Rely on the project-wide LICENSE file.

--- riak-client/erl_src/riak_kv_test_backend.erl
+++ riak-client/erl_src/riak_kv_test_backend.erl
@@ -2,21 +2,6 @@
 %%
 %% riak_kv_test_backend: storage engine based on ETS tables
 %%
-%% Copyright (c) 2010-2011 Sean Cribbs and Basho Technologies, Inc.
-%%
-%% This file is provided to you under the Apache License,
-%% Version 2.0 (the "License"); you may not use this file
-%% except in compliance with the License.  You may obtain
-%% a copy of the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing,
-%% software distributed under the License is distributed on an
-%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
-%% specific language governing permissions and limitations
-%% under the License.
 %%
 %% -------------------------------------------------------------------
 
