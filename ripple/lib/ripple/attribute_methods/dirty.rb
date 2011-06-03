2375f5c879a5fa061d8dc283edf5e52fe78f8fac
Sean Cribbs <seancribbs@gmail.com>
2011-06-02 21:04:19 -0400

    Fix order of callbacks so that previous_changes is available to callbacks.

--- ripple/lib/ripple/attribute_methods/dirty.rb
+++ ripple/lib/ripple/attribute_methods/dirty.rb
@@ -22,7 +22,7 @@ module Ripple
       include ActiveModel::Dirty
 
       # @private
-      def save(*args)
+      def really_save(*args)
         if result = super
           @previously_changed = changes
           changed_attributes.clear
--- ripple/spec/ripple/attribute_methods/dirty_spec.rb
+++ ripple/spec/ripple/attribute_methods/dirty_spec.rb
@@ -1,14 +1,29 @@
 require File.expand_path("../../../spec_helper", __FILE__)
 
 describe Ripple::AttributeMethods::Dirty do
-  describe "#changed?" do
-    let(:company)    { Company.new }
+  let(:company)    { Company.new }
+  let(:ceo)        { CEO.new(:name => 'John Doe') }
+  let(:department) { Department.new(:name => 'Marketing') }
+  let(:manager)    { Manager.new(:name => 'Billy Willy') }
+  let(:invoice)    { Invoice.new }
 
-    it "should capture previous changes when saving" do
+  describe "previous_changes" do
+    before do
       company.robject.stub!(:store).and_return(true)
       company.name = 'Fizz Buzz, Inc.'
+    end
+    
+    it "should capture previous changes when saving" do
       company.save
       company.previous_changes.should include('name')
+    end
+
+    it "should make previous changes available to after callbacks" do
+      class << company
+        after_save {|c| c['pc'] = previous_changes }
+      end
+      company.save
+      company['pc'].should include('name')
     end
   end
 end
