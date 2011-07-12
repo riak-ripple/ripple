49ca69a02640412c7b57d031cb745324fe7aac72
Myron Marston <myron.marston@gmail.com>
2011-07-12 09:47:45 -0700

    Allow rspec metadata to be specified as raw symbols (rather than needing symbol => true).
    This is really nice for spec focusing; `:focus` can be used alone.

--- ripple/spec/spec_helper.rb
+++ ripple/spec/spec_helper.rb
@@ -18,7 +18,8 @@ RSpec.configure do |config|
     $test_server.recycle if $test_server
   end
 
-  config.filter_run :focus => true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus
   config.run_all_when_everything_filtered = true
   config.debug = true
   config.include Ripple::Conflict::TestHelper
