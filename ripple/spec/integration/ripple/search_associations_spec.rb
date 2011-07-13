bdacf06709e2a3ca64081f4c56f02c2f315a3007
Myron Marston <myron.marston@gmail.com>
2011-07-13 10:37:22 -0700

    Fix spec helper requires.

--- ripple/spec/integration/ripple/search_associations_spec.rb
+++ ripple/spec/integration/ripple/search_associations_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../../../spec_helper", __FILE__)
+require 'spec_helper'
 
 describe "Ripple Search Associations" do
   require 'support/test_server'
--- ripple/spec/ripple/associations/many_stored_key_proxy_spec.rb
+++ ripple/spec/ripple/associations/many_stored_key_proxy_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../../../spec_helper", __FILE__)
+require 'spec_helper'
 
 describe Ripple::Associations::ManyStoredKeyProxy do
   require 'support/models/transactions'
