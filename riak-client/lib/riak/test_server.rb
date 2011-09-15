7847fb4e665be48f3759a31c7dad3ca60a45ad1f
Daniel Reverri <reverri@gmail.com>
2011-09-15 10:45:43 -0700

    Use the @temp_dir as the test server's working directory

--- riak-client/lib/riak/test_server.rb
+++ riak-client/lib/riak/test_server.rb
@@ -177,6 +177,7 @@ module Riak
           line.sub!(/(RUNNER_USER=)(.*)/, '\1')
           line.sub!(/(RUNNER_LOG_DIR=)(.*)/, '\1' + @temp_log)
           line.sub!(/(PIPE_DIR=)(.*)/, '\1' + @temp_pipe)
+          line.sub!(/^cd \$RUNNER_BASE_DIR/, "cd #{@temp_dir}")
           if line.strip == "RUNNER_BASE_DIR=${RUNNER_SCRIPT_DIR%/*}"
             line = "RUNNER_BASE_DIR=#{File.expand_path("..",@bin_dir)}\n"
           end
