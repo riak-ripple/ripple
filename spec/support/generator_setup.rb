f28a4b940708a2df93566f59b340cef11f02a8b7
Sean Cribbs <sean@basho.com>
2012-03-01 07:16:40 -0500

    Fix for warnings on Rails 3.2 caused by ammeter.

--- spec/support/generator_setup.rb
+++ spec/support/generator_setup.rb
@@ -1,6 +1,18 @@
 require 'ammeter/init'
 require 'fileutils'
 
+# Silences warnings on Rails 3.2 that are caused by ammeter. Remove
+# when ammeter gem version is bumped. See commit:
+# https://github.com/alexrothenberg/ammeter/commit/08e27fbcd2e710f2129f65e1dac1047eb70542ee
+if Ammeter::RSpec::Rails::GeneratorExampleGroup.const_defined?(:InstanceMethods)
+  module Ammeter::RSpec::Rails::GeneratorExampleGroup
+     def invoke_task name
+       capture(:stdout) { generator.invoke_task(generator_class.all_tasks[name.to_s]) }
+     end
+     remove_const :InstanceMethods
+  end
+end
+
 module GeneratorSetup
   include FileUtils
   def self.included(group)
