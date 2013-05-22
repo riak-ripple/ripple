e43592cd51b2de750f0258024c643a6f8b5e6e73
Bryce Kerley <bkerley@brycekerley.net>
2013-05-22 19:53:52 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ripple::Application.config.secret_key_base = '895d1f0e3c4ec5c50e77f0a55be54a4f83138ab8a2a2fb4d47fb319a590bc94e5db41e6595212691c8ad59c34ddb5f9b937fa0966ec2f3ab37ee1fad92f4b85b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ripple::Application.config.session_store :cookie_store, key: '_ripple_session'
