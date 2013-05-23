633602181acc01b162e4a7411b83f65c02f47c4e
Bryce Kerley <bkerley@brycekerley.net>
2013-05-23 01:06:38 -0400

    disable acriveresource, actionmailer, and activerecord

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,13 +14,13 @@ Ripple::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations
-  config.active_record.migration_error = :page_load
+  # config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
