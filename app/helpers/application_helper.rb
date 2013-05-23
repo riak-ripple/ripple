61d62af167d0e6fa47d7ab204d65bc5c99c370d8
Bryce Kerley <bkerley@brycekerley.net>
2013-05-23 12:33:41 -0400

    add protip helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def source_dive(&blk)
+    content_tag :div, class: 'protip source_dive' do
+      content_tag(:h3, 'Source Diving') +
+      content_tag(:div, class: 'protip-content', &blk)
+    end
+  end
 end
