6fdf49bae345a504353ad8e00a8c5fd9de558de6
Bryce Kerley <bkerley@brycekerley.net>
2013-05-23 13:16:48 -0400

    try another asset constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Ripple::Application.routes.draw do
   root to: 'docs#show', id: 'index'
-  get '/:id', to: 'docs#show', constraints: {id: /\w+/}
+  get '/:id', to: 'docs#show', constraints: {id: /[^a][^s][^s][^e][^t]/}
 end
